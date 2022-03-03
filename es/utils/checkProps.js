2baf882380b3634aac77ae89a12b2efc380a10df
Alex Balashov <alexander_balashov@hms.harvard.edu>
2022-03-03 14:31:26 -0500

    Major cleanup, simplify build down to basics...
    Get rid of gulp, uglify, etc.
    Get rid of /dist/ and replace with /es/ like on SPC.

--- es/utils/checkProps.js
+++ es/utils/checkProps.js
@@ -1,14 +1,15 @@
-'use strict';
+"use strict";
 
 // This module provides a centralized place for
 // runtime checking that the props passed to React Infinite
 // make the minimum amount of sense.
-
 var React = global.React || require('react');
+
 var _isFinite = require('lodash.isfinite');
 
 module.exports = function (props) {
   var rie = 'Invariant Violation: ';
+
   if (!(props.containerHeight || props.useWindowAsScrollContainer)) {
     throw new Error(rie + 'Either containerHeight or useWindowAsScrollContainer must be provided.');
   }
--- es/utils/infiniteHelpers.js
+++ es/utils/infiniteHelpers.js
@@ -1,44 +1,50 @@
-'use strict';
+"use strict";
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+var _excluded = ["containerHeight", "preloadBatchSize", "preloadAdditionalHeight", "handleScroll", "onInfiniteLoad"];
 
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
 
 var ConstantInfiniteComputer = require('../computers/constantInfiniteComputer.js');
+
 var ArrayInfiniteComputer = require('../computers/arrayInfiniteComputer.js');
+
 var scaleEnum = require('./scaleEnum');
+
 var React = global.React || require('react');
+
 var window = require('./window');
 
 function createInfiniteComputer(data, children) {
   var computer;
-  var numberOfChildren = React.Children.count(children);
+  var numberOfChildren = React.Children.count(children); // This should be guaranteed by checkProps
 
-  // This should be guaranteed by checkProps
   if (Array.isArray(data)) {
     computer = new ArrayInfiniteComputer(data, numberOfChildren);
   } else {
     computer = new ConstantInfiniteComputer(data, numberOfChildren);
   }
-  return computer;
-}
 
-// Given the scrollTop of the container, computes the state the
+  return computer;
+} // Given the scrollTop of the container, computes the state the
 // component should be in. The goal is to abstract all of this
 // from any actual representation in the DOM.
 // The window is the block with any preloadAdditionalHeight
 // added to it.
+
+
 function recomputeApertureStateFromOptionsAndScrollTop(_ref, scrollTop) {
   var preloadBatchSize = _ref.preloadBatchSize,
       preloadAdditionalHeight = _ref.preloadAdditionalHeight,
       infiniteComputer = _ref.infiniteComputer;
-
   var blockNumber = preloadBatchSize === 0 ? 0 : Math.floor(scrollTop / preloadBatchSize),
       blockStart = preloadBatchSize * blockNumber,
       blockEnd = blockStart + preloadBatchSize,
       apertureTop = Math.max(0, blockStart - preloadAdditionalHeight),
       apertureBottom = Math.min(infiniteComputer.getTotalScrollableHeight(), blockEnd + preloadAdditionalHeight);
-
   return {
     displayIndexStart: infiniteComputer.getDisplayIndexStart(apertureTop),
     displayIndexEnd: infiniteComputer.getDisplayIndexEnd(apertureBottom)
@@ -52,13 +58,14 @@ function generateComputedProps(props) {
       preloadAdditionalHeight = props.preloadAdditionalHeight,
       handleScroll = props.handleScroll,
       onInfiniteLoad = props.onInfiniteLoad,
-      oldProps = _objectWithoutProperties(props, ['containerHeight', 'preloadBatchSize', 'preloadAdditionalHeight', 'handleScroll', 'onInfiniteLoad']);
+      oldProps = _objectWithoutProperties(props, _excluded);
 
   var newProps = {};
   containerHeight = typeof containerHeight === 'number' ? containerHeight : 0;
   newProps.containerHeight = props.useWindowAsScrollContainer ? window.innerHeight : containerHeight;
 
   newProps.handleScroll = handleScroll || function () {};
+
   newProps.onInfiniteLoad = onInfiniteLoad || function () {};
 
   var defaultPreloadBatchSizeScaling = {
@@ -69,7 +76,7 @@ function generateComputedProps(props) {
 
   if (typeof preloadBatchSize === 'number') {
     newProps.preloadBatchSize = preloadBatchSize;
-  } else if ((typeof batchSize === 'undefined' ? 'undefined' : _typeof(batchSize)) === 'object' && batchSize.type === scaleEnum.CONTAINER_HEIGHT_SCALE_FACTOR) {
+  } else if (_typeof(batchSize) === 'object' && batchSize.type === scaleEnum.CONTAINER_HEIGHT_SCALE_FACTOR) {
     newProps.preloadBatchSize = newProps.containerHeight * batchSize.amount;
   } else {
     newProps.preloadBatchSize = 0;
@@ -80,9 +87,10 @@ function generateComputedProps(props) {
     amount: 1
   };
   var additionalHeight = preloadAdditionalHeight && preloadAdditionalHeight.type ? preloadAdditionalHeight : defaultPreloadAdditionalHeightScaling;
+
   if (typeof preloadAdditionalHeight === 'number') {
     newProps.preloadAdditionalHeight = preloadAdditionalHeight;
-  } else if ((typeof additionalHeight === 'undefined' ? 'undefined' : _typeof(additionalHeight)) === 'object' && additionalHeight.type === scaleEnum.CONTAINER_HEIGHT_SCALE_FACTOR) {
+  } else if (_typeof(additionalHeight) === 'object' && additionalHeight.type === scaleEnum.CONTAINER_HEIGHT_SCALE_FACTOR) {
     newProps.preloadAdditionalHeight = newProps.containerHeight * additionalHeight.amount;
   } else {
     newProps.preloadAdditionalHeight = 0;
